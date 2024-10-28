4ff65b487254a0c8c4003e42e749cc4197e483b8
wwwacedo <eng.marianammacedo@gmail.com>
2024-10-28 10:59:59 -0300

    feat: menu Aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,14 +1,55 @@
-import Image from 'next/image'
-import styles from './aside.module.css'
-
 import logo from './logo.png'
 import Link from 'next/link'
+
+import AsideLink from '../AsideLink'
+import Image from 'next/image'
+import { Feed } from '../icons/Feed'
+import { Account } from '../icons/Account'
+import { Info } from '../icons/Info'
+import { Login } from '../icons/Login'
+import { Button } from '../Button'
+import styles from './aside.module.css'
 
 export const Aside = () => {
     return (<aside className={styles.aside}>
         {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-        <Link href="/">
-            <Image src={logo} alt="Logo da Code Connect"/>
-        </Link>
+				<nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <Image src={logo} alt="Logo da Code Connect" />
+                    </Link>
+                </li>
+                <li>
+                    <Button href="/publish" outline>
+                        Publicar
+                    </Button>
+                </li>
+                <li>
+                    <AsideLink href="/">
+                        <Feed />
+                        Feed
+                    </AsideLink>
+                </li>
+                <li>
+                    <AsideLink href="/profile">
+                        <Account />
+                        Perfil
+                    </AsideLink>
+                </li>
+                <li>
+                    <AsideLink href="/about">
+                        <Info />
+                        Sobre nós
+                    </AsideLink>
+                </li>
+                <li>
+                    <AsideLink href="/login?">
+                        <Login />
+                        Login
+                    </AsideLink>
+                </li>
+            </ul>
+        </nav>
     </aside>)
 }
