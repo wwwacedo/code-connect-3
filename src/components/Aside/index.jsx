f92cba4637434850699469eebcaebab2ff5623f9
wwwacedo <eng.marianammacedo@gmail.com>
2024-10-29 11:20:14 -0300

    feat: adicionando login e logout no aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -10,46 +10,61 @@ import { Login } from '../icons/Login'
 import { Button } from '../Button'
 import styles from './aside.module.css'
 
-export const Aside = () => {
-    return (<aside className={styles.aside}>
-        {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-				<nav>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <Image src={logo} alt="Logo da Code Connect" />
-                    </Link>
-                </li>
-                <li>
-                    <Button href="/publish" outline>
-                        Publicar
-                    </Button>
-                </li>
-                <li>
-                    <AsideLink href="/">
-                        <Feed />
-                        Feed
-                    </AsideLink>
-                </li>
-                <li>
-                    <AsideLink href="/profile">
-                        <Account />
-                        Perfil
-                    </AsideLink>
-                </li>
-                <li>
-                    <AsideLink href="/about">
-                        <Info />
-                        Sobre nós
-                    </AsideLink>
-                </li>
-                <li>
-                    <AsideLink href="/login?">
-                        <Login />
-                        Login
-                    </AsideLink>
-                </li>
-            </ul>
-        </nav>
-    </aside>)
+import { options } from '@/app/api/auth/[...nextauth]/options'
+import { getServerSession } from 'next-auth'
+
+export const Aside = async () => {
+
+	const session = await getServerSession(options);
+
+	return (<aside className={styles.aside}>
+		{/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
+		<nav>
+			<ul>
+				<li>
+					<Link href="/">
+						<Image src={logo} alt="Logo da Code Connect" />
+					</Link>
+				</li>
+				<li>
+					<Button href="/publish" outline>
+						Publicar
+					</Button>
+				</li>
+				<li>
+					<AsideLink href="/">
+						<Feed />
+						Feed
+					</AsideLink>
+				</li>
+				<li>
+					<AsideLink href="/profile">
+						<Account />
+						Perfil
+					</AsideLink>
+				</li>
+				<li>
+					<AsideLink href="/about">
+						<Info />
+						Sobre nós
+					</AsideLink>
+				</li>
+				{ !session && <li>
+					<AsideLink href="/api/auth/signin">
+						<Login />
+						Login
+					</AsideLink>
+					</li>
+				}
+				{ session && <li>
+					<AsideLink href="/api/auth/signout">
+						<Login />
+						Logout
+					</AsideLink>
+					</li>
+				}
+				
+			</ul>
+		</nav>
+	</aside>)
 }
