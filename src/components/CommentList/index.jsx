287723ecc376c60d9c0a8b1b62670f5d8bea1028
wwwacedo <eng.marianammacedo@gmail.com>
2024-12-11 07:52:24 -0300

    resolvendo erro Each child in a list should have a unique "key" prop

--- src/components/CommentList/index.jsx
+++ src/components/CommentList/index.jsx
@@ -5,18 +5,19 @@ import { Replies } from "../Replies"
 import styles from './commentlist.module.css'
 
 export const CommentList = ({ comments }) => {
-    return (
-        <section className={styles.comments}>
-            <h2>
-                Comentários
-            </h2>
-            <ul>
-                {comments.map( comment => <li>
-                    <Comment comment={comment} key={comment.id}/>
-                    <ReplyModal comment={comment}/>
-                    <Replies comment={comment}/>
-                </li> )}
-            </ul>
-        </section>
-    )
+	return (
+		<section className={styles.comments}>
+			<h2>
+				Comentários
+			</h2>
+			<ul>
+				{comments.map(comment =>
+					<li key={comment.id}>
+						<Comment comment={comment} key={comment.id} />
+						<ReplyModal comment={comment} />
+						<Replies comment={comment} />
+					</li>)}
+			</ul>
+		</section>
+	)
 }
